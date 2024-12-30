13188f6972ed0ac7dc962d4ecdd4e30fa67e4ca2
aleuxgo <madalex-nb@yandex.ru>
2024-12-30 20:34:50 +0400

    finished new homework

--- main.py
+++
@@ -1,9 +0,0 @@
-from src.masks import get_mask_account, get_mask_card_number
-
-print("Введите номер карты (16 цифр без пробелов): ")
-user_card_number = input()
-print(get_mask_card_number(user_card_number))
-
-print("Введите номер счета (20 цифр без пробела): ")
-user_account = input()
-print(get_mask_account(user_account))
--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,12 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция  принимает на вход номер карты и возвращает маску номера"""
 
-    if card_number.isdigit() and len(card_number) == 16:
-        mask_card_number = card_number[:4] + " " + card_number[4:6] + "** " + 4 * "*" + " " + card_number[-4:]
-        return mask_card_number
-    else:
-        return "Введен некорректный номер карты"
+    mask_card_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+    return mask_card_number
 
 
-def get_mask_account(account: str) -> str:
+def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает маску номера"""
-    if account.isdigit() and len(account) == 20:
-        mask_account = "**" + account[-4:]
-        return mask_account
-    else:
-        return "Введен некорректный номер счета"
+
+    mask_account = "**" + account_number[-4:]
+    return mask_account
