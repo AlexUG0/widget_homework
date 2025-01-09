4e17a6ace966bbeb33ee2f9988adcee270e14894
AlexUG0 <aleksejugolkov177@gmail.com>
2025-01-09 21:01:47 +0400

    file processing is done

---
+++ src/processing.py
@@ -0,0 +1,14 @@
+def filter_by_state(transaction_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция принимает на вход список транзакций и возвращает список, отфильтрованный по состоянию"""
+
+    state_filter_list = []
+    for transaction in transaction_list:
+        if transaction["state"] == state:
+            state_filter_list.append(transaction)
+    return state_filter_list
+
+
+def sort_by_date(transaction_list: list[dict]) -> list[dict]:
+    """Функция принимает на вход список транзакций и возвращает список, отсортированный по дате"""
+    date_sort_list = sorted(transaction_list, key=lambda transaction: transaction["date"], reverse=True)
+    return date_sort_list
