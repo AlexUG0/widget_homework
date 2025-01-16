70aabde65b84a429c1af7752047c918c285d73c8
AlexUG0 <aleksejugolkov177@gmail.com>
2025-01-16 19:19:48 +0400

    init homework 10_2

---
+++ tests/test_processing.py
@@ -0,0 +1,6 @@
+def test_filter_by_state():
+    filter_by_state() ==
+
+
+def test_sort_by_date():
+    sort_by_date() ==
---
+++ tests/test_widget.py
@@ -0,0 +1,6 @@
+def test_mask_account_card():
+    mask_account_card() ==
+
+
+def test_get_date():
+    get_date() ==
