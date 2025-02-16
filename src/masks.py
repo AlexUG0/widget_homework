b432edcda3a2e1d2b7ff9dd2afdd8821e394a4c5
AlexUG0 <aleksejugolkov177@gmail.com>
2025-02-16 20:59:09 +0400

    homework begin

--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,35 @@
+import os
+import logging
+
+from config import PATH_HOME
+
+# Основная конфигурация logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename=os.path.join(PATH_HOME, "logs", "masks.log"),  # Запись логов в файл
+                    filemode='w')  # Перезапись файла при каждом запуске
+logger = logging.getLogger('masks.py')
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция  принимает на вход номер карты и возвращает маску номера"""
+
+    logger.info("Маскировка банковской карты")
     if len(card_number) == 16 and card_number.isdigit():
         mask_card_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
         return mask_card_number
     else:
+        logger.warning("Введены некорректные данные")
         return "Неверный формат ввода"
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает маску номера"""
 
+    logger.info("Маскировка банковской счета")
     if len(account_number) == 20 and account_number.isdigit():
         mask_account = "**" + account_number[-4:]
         return mask_account
     else:
+        logger.warning("Введены некорректные данные")
         return "Неверный формат ввода"
