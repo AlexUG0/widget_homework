e5a71a446446247c5ab1a1ca3fa67606345364a1
AlexUG0 <aleksejugolkov177@gmail.com>
2025-03-01 10:34:32 +0400

    homework started

---
+++ src/searchig.py
@@ -0,0 +1,20 @@
+import re
+from typing import Dict, List
+
+
+def filter_transactions(transactions: List[Dict], search_string: str) -> List[Dict]:
+    pattern = re.compile(re.escape(search_string), re.IGNORECASE)  # Компилируем шаблон
+    filtered_transactions = [
+        transaction for transaction in transactions if pattern.search(transaction.get("description", ""))
+    ]
+    return filtered_transactions
+
+
+def count_transactions_by_category(transactions: List[Dict], categories: List[str]) -> Dict[str, int]:
+    category_count = {category: 0 for category in categories}
+    for transaction in transactions:
+        description = transaction.get("description", "").lower()
+        for category in categories:
+            if category.lower() in description:
+                category_count[category] += 1
+    return category_count
